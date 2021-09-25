990e4cc26a08b4505029f98121c3e08e8a17392d
June103310110 <32012425+June103310110@users.noreply.github.com>
2021-09-26 00:54:58 +0800

    變更flask run host

--- 03_msg_by_usr_msg.py
+++ 03_msg_by_usr_msg.py
@@ -74,4 +74,4 @@ def handle_message(event):
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
