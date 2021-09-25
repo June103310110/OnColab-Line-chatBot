86c50fd0f93517bb8860168be5da885ed2d0a14a
June103310110 <32012425+June103310110@users.noreply.github.com>
2021-09-25 22:48:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+      # get user id when reply
+    user_id = event.source.user_id
+    print("user_id =", user_id)
+    
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
