f534faefb9f27e57726105c202c56218f49aa657
June103310110 <32012425+June103310110@users.noreply.github.com>
2021-09-26 11:41:00 +0800

    Update 04_simple_reply_img.py

--- 04_simple_reply_img.py
+++ 04_simple_reply_img.py
@@ -69,4 +69,4 @@ def handle_message(event):
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host=0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
