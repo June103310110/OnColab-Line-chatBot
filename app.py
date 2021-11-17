bc7a3e8ab0e0e5b1397eec8dd0b0856132553893
June103310110 <32012425+June103310110@users.noreply.github.com>
2021-11-17 11:18:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,21 @@
 from flask import Flask, request, abort
-import string
-from flask import url_for, render_template 
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError, LineBotApiError
+    InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, Video, ExternalLink, PostbackEvent
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
-# from linebot.models import ImagemapSendMessage, BaseSize, URIImagemapAction, MessageImagemapAction, ImagemapArea
-from linebot.models import TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
-import random
-import glob
-import os
-
-try:
-    os.makedirs('./static/user_upload', exist_ok=False)
-    os.makedirs('./templates', exist_ok=False)
-except FileExistsError:
-    pass
-    
 app = Flask(__name__)
-PEOPLE_FOLDER = os.path.join('static', 'user_upload')
-app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
-
-# 設定chatbot
-line_bot_api = LineBotApi('Channel access token')
-handler = WebhookHandler('Channel secret')
+line_bot_api = LineBotApi('Yhw5V0DuMqmWNsBsD2qOaAQAcCN6tH8hNnMDL4EySJzV9c4fara9/fP2Q982Cc1726FA/V8u4Nt4ZY9apNz4A/X6zMHGkCWh38zNzlqBaA8vFMsEzb6huWaU4PqXMv7LJJPgeJuyFFnm3bn56d8H0wdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('bf87a1994230b40b009a542b1747dc3f')
 
 # 推給你自己 
-line_bot_api.push_message('Your user ID ', TextSendMessage(text='(後臺訊息)啟動豆芽探索共學ECHO機器人!'))
-# 推給某個User
-# line_bot_api.push_message('UserID', TextSendMessage(text='(後臺訊息)啟動豆芽探索共學ECHO機器人!'))
+line_bot_api.push_message('U5eb2d6c5020d6fe62e4f1d1e0f15e406', TextSendMessage(text='(後臺訊息)啟動豆芽探索共學ECHO機器人!'))
 
 
 @app.route("/callback", methods=['POST'])
@@ -56,73 +37,11 @@ def callback():
     return 'OK'
 
 
-button_template_message =ButtonsTemplate(
-                        thumbnail_image_url="https://i.imgur.com/d3vfgZP.png",
-                        title='Menu', 
-                        text='Please select',
-                        ratio="1.51:1",
-                        image_size="cover",
-                        actions=[
-                            MessageTemplateAction(
-                                label='message會回傳你好', text='你好'
-                            ),
-                            URITemplateAction(
-                                label='uri可回傳網址', uri='https://hackmd.io/DPLQVfzFS3yAs4ZcpY56NQ?view'
-                            )
-                        ]
-                    )
-
-# 取得使用者上傳的圖片
-@handler.add(MessageEvent)
-def handle_img_message(event):
-    
-    user_id = event.source.user_id
-    print("user_id =", user_id)
-    print(event.message.type)
-    if event.message.type=='image':
-        image_name = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(4))
-        image_content = line_bot_api.get_message_content(event.message.id)
-        image_name = image_name.upper()+'.jpg'
-        path='./static/user_upload/'+user_id+'_'+image_name
-        print(path)
-        with open(path, 'wb') as fd:
-            for chunk in image_content.iter_content():
-                fd.write(chunk)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='收到照片，分析中!'))
-
-                
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-      # get user id when reply
-    user_id = event.source.user_id
-    print("user_id =", user_id)
-    print(event.message.type)
-    reply_msg = event.message.text+'\nyour User ID is '+user_id+\
-                    ' \n輸入「你好」會啟動reply_message回復「不錯喔」'
-    
-    if event.message.text=='你好':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='不錯喔'))
-
-        
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_msg))
-        try:
-    #     alt_text 因template只能夠在手機上顯示，因此在PC版會使用alt_Text替代
-            line_bot_api.push_message(user_id, TemplateSendMessage(alt_text="Template Example", 
-                                                                   template=button_template_message))
-        except LineBotApiError as e:
-            # error handle
-            raise e
-
-        
-        
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 import os
 if __name__ == "__main__":
