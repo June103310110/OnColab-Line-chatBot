74cab9426ec49138462cd8d80a4ca7acb2ba59d5
June103310110 <32012425+June103310110@users.noreply.github.com>
2021-11-17 14:24:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+import pandas as pd
 
 app = Flask(__name__)
 line_bot_api = LineBotApi('Yhw5V0DuMqmWNsBsD2qOaAQAcCN6tH8hNnMDL4EySJzV9c4fara9/fP2Q982Cc1726FA/V8u4Nt4ZY9apNz4A/X6zMHGkCWh38zNzlqBaA8vFMsEzb6huWaU4PqXMv7LJJPgeJuyFFnm3bn56d8H0wdB04t89/1O/w1cDnyilFU=')
@@ -47,23 +48,82 @@ def handle_message(event):
     user_id = event.source.user_id # 哪一個使用者傳的訊息
     profile = line_bot_api.get_profile(user_id) # 取得使用者個人資料
     user_name = profile.display_name # 他的帳號名稱
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+
+    global df
+    global key
+    
+    
+    if key == '註冊' and event.message.text.isdigit():
+        msg = '收到您的卡號，幫您註冊...'
+        line_bot_api.push_message(user_id, TextSendMessage(text=msg)) 
+        
+        if user_name in df.profileName.values:
+            index = df['profileName'] == user_name
+            df.loc[index, 'card'] = event.message.text
+            msg += str(df.loc[index, :].values[0])
+        else:
+            lis = [user_name, event.message.text]
+            df.loc[len(df)] = lis
+            msg += str(lis)
+
+        line_bot_api.push_message(user_id, TextSendMessage(text=msg)) 
+        key = None
+    elif event.message.text == '註冊':
+        msg = '為您註冊資料庫，請輸入(更新)卡號...'
+        line_bot_api.push_message(user_id, TextSendMessage(text=msg)) 
+        key = '註冊'
+    
+    if event.message.text == '我的卡號':
+        key = '我的卡號'
+    if key == '我的卡號':
+        if user_name in df.profileName.values:
+            user_card = df[df['profileName'] == user_name]['card'][0]
+            msg = user_name+':'+str(user_card)
+            line_bot_api.push_message(user_id, TextSendMessage(text=msg)) 
+            key = None
     
 
-    if event.message.text == 'hit':
-        result = os.popen("curl -X POST 'https://class.aiacademy.tw/enter_logs/ajax_logs2.php'\
-          --data-raw 'room_id=17&card_id=3995236992&bye=F'").read()
 
-        a = result.split("data")[1]
-        b = a.split(':')[2].split(',')[0]
-        b = b[1:-1]
-        d = bytes(b, encoding='utf-8')
-        
+    if event.message.text == 'hit':
+        msg = '請稍等，為您聯繫資料庫確認打卡資訊...，'
         line_bot_api.push_message(user_id,
-          TextSendMessage(text=d.decode('unicode_escape'))) 
+          TextSendMessage(text=msg)) 
+
+        if user_name in df['profileName'].values:
+            index = df[df['profileName'] == user_name]
+            user_card = df[df['profileName'] == user_name]['card'][0]
+        
+            result = os.popen("curl -X POST 'https://class.aiacademy.tw/enter_logs/ajax_logs2.php'\
+              --data-raw 'room_id=17&card_id={user_card}&bye=F'".format(user_card=user_card)).read()
+
+            a = result.split("data")[1]
+            b = a.split(':')[2].split(',')[0]
+            b = b[1:-1]
+            d = bytes(b, encoding='utf-8')
+
+            if str(d.decode('unicode_escape')) == str(user_card):
+                msg = '打卡失敗，卡號錯誤...你目前註冊的卡號是{user_card}'.format(user_card=user_card)
+            else:
+                msg = d.decode('unicode_escape')+'打卡成功'
+            line_bot_api.push_message(user_id, TextSendMessage(text=msg)) 
+        else:
+            msg = '你尚未註冊卡號'
+            line_bot_api.push_message(user_id,
+              TextSendMessage(text=msg)) 
+    else:
+      pass
+    
+    df.to_csv('aiaMenber.csv', index=False)
+    
 
 if __name__ == "__main__":
+    try:
+      df = pd.read_csv('aiaMenber.csv')
+    except FileNotFoundError:
+      df = pd.DataFrame([], columns=['profileName', 'card'])
+      df.to_csv('aiaMenber.csv', index=False)
+
+    key = None
+    # run_webApi()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
